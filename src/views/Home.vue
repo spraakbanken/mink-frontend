b76cc3767545e80859a8b28bd82cfcd4b34bb5a7
Arild Matsson <arild.matsson@gu.se>
2021-09-09 18:12:45 +0200

    Router, Vuex

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Min språkbank</h1>
+  {{ user }}
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+const user = computed(() => store.state.user);
+</script>
+
+<style>
+</style>
