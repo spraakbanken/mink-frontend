af2081dc20d494f96825c29f4faf6678b2223271
Arild Matsson <arild.matsson@gu.se>
2021-09-10 15:15:48 +0200

    Local storage, logout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
+import { computed } from "@vue/reactivity";
+import router from "./router";
+import store from "./store";
+
+const user = computed(() => store.state.user);
+
+function logout() {
+  store.commit("setUser", null);
+  router.push("/login");
+}
 </script>
 
 <template>
   <div>
     <router-view></router-view>
   </div>
+  <footer>
+    <button v-if="user" @click="logout">Logga ut</button>
+  </footer>
 </template>
 
 <style>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import { createStore } from "vuex";
+import VuexPersistence from "vuex-persist";
 
 export default createStore({
+  plugins: [new VuexPersistence().plugin],
   state() {
     return {
       user: null,
