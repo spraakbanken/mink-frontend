e71e9b2f7a50c596809d370e0606334f40b782ae
Arild Matsson <arild.matsson@gu.se>
2022-07-26 18:48:20 +0200

    Mink or no Mink

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,16 +34,38 @@ watchEffect(() => {
           disposal of researchers everywhere, and take pride in providing free
           and open access where possible.
         </p>
+
         <p class="mb-2">
-          <strong>Mink</strong> is our effort to bring more of our language
-          resource tools into the hands of researchers. You can use Mink to run
+          <strong>Mink</strong> is our effort to bring the resource creation
+          toolchain into the hands of researchers. You can use Mink to run
           automatic annotation models on texts that you have collected yourself.
           The resulting resource can be downloaded, shared, and used directly
           for concordance search in Korp.
         </p>
+
         <div class="text-center p-4 text-xl">
           <LoginButton class="mr-4" />
         </div>
+
+        <p class="mb-2">
+          <strong>Mink or no Mink?</strong>
+          Make sure to first get acquainted to any existing resources related to
+          your interests! Språkbanken's growing collection of language resources
+          can be browsed on the
+          <a
+            href="https://spraakbanken.gu.se/en/resources"
+            title="Språkbanken Text: Resources"
+            >Resources</a
+          >
+          section of our website. At a larger scale, Språkbanken forms a part of
+          the CLARIN ERIC, whose collected assortment of resource can be browsed
+          in the
+          <a
+            href="https://vlo.clarin.eu/"
+            title="CLARIN Virtual Language Observatory"
+            >Virtual Language Observatory</a
+          >.
+        </p>
       </div>
 
       <div class="lg:w-1/2 p-4">
@@ -58,10 +80,10 @@ watchEffect(() => {
           Sparv.
         </Section>
 
-        <Section title="Upload your text data" class="mt-0">
-          Add your word-processor documents, plain <em>txt</em> files or XML
-          data. When using XML, annotations are added as attributes, keeping the
-          structure intact.
+        <Section title="Deposit your text" class="mt-0">
+          Upload your word-processor documents, plain <em>txt</em> files or XML
+          data. When using XML, annotation output is added as attributes,
+          keeping the structure intact.
         </Section>
 
         <Section title="Explore in Korp" class="mt-0">
@@ -74,7 +96,7 @@ watchEffect(() => {
           >.
         </Section>
 
-        <Section title="Collaboration and sharing" class="mt-0">
+        <Section title="Share and collaborate" class="mt-0">
           Invite fellow project members to manage the process. Share the
           finished resource to the research community.
         </Section>
