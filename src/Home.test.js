6297fdaab7c41a1ce868b8e8480725c4ce8eccee
Arild Matsson <arild.matsson@gu.se>
2023-01-04 15:48:14 +0100

    Fix Home test

--- src/Home.test.js
+++ src/Home.test.js
@@ -3,13 +3,12 @@ import { test } from "vitest";
 import i18n from "@/i18n/i18n";
 import Home from "./Home.vue";
 import router from "@/router";
-import store from "@/store";
 import { FontAwesomeIcon } from "@/fontawesome";
 
 test("frontpage", () => {
   const { getByText } = render(Home, {
     global: {
-      plugins: [i18n, router, store],
+      plugins: [i18n, router],
       components: { icon: FontAwesomeIcon },
     },
   });
