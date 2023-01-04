0b011460fabfa451272c02d84b4b5d768d0c9e5d
Arild Matsson <arild.matsson@gu.se>
2023-01-04 17:28:32 +0100

    Make whole upload area clickable

--- src/corpus/sources/SourceUpload.vue
+++ src/corpus/sources/SourceUpload.vue
@@ -1,43 +1,43 @@
 <template>
   <Filedrop @drop="fileHandler">
     <slot />
-    <div
-      :class="
-        variant == 'primary'
-          ? [
-              'bg-blue-50',
-              'border-blue-100',
-              'dark:bg-blue-200',
-              'dark:border-blue-300',
-              'dark:text-blue-800',
-            ]
-          : [
-              'bg-zinc-100',
-              'border-zinc-200',
-              'dark:bg-zinc-700',
-              'dark:border-zinc-500',
-            ]
-      "
-      class="border-dashed border-4"
-    >
-      <label class="absolute uppercase opacity-70 text-sm font-bold p-1">
-        {{ $t("addFile") }}
-      </label>
-      <div class="p-8">
-        <div class="flex flex-col justify-center items-center opacity-70">
-          <label for="file-input" class="cursor-pointer">
+    <label for="file-input" class="cursor-pointer">
+      <div
+        :class="
+          variant == 'primary'
+            ? [
+                'bg-blue-50',
+                'border-blue-100',
+                'dark:bg-blue-200',
+                'dark:border-blue-300',
+                'dark:text-blue-800',
+              ]
+            : [
+                'bg-zinc-100',
+                'border-zinc-200',
+                'dark:bg-zinc-700',
+                'dark:border-zinc-500',
+              ]
+        "
+        class="border-dashed border-4"
+      >
+        <span class="absolute uppercase opacity-70 text-sm font-bold p-1">
+          {{ $t("addFile") }}
+        </span>
+        <div class="p-8">
+          <div class="flex flex-col justify-center items-center opacity-70">
             {{ $t("dragANDdrop") }}
-          </label>
-          <input
-            id="file-input"
-            type="file"
-            class="hidden"
-            multiple
-            @change="(event) => fileHandler(event.target.files)"
-          />
+            <input
+              id="file-input"
+              type="file"
+              class="hidden"
+              multiple
+              @change="(event) => fileHandler(event.target.files)"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </label>
   </Filedrop>
 </template>
 
