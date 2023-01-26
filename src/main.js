5326ace55d2e68ea7271ff47b2796bf2b6bc7281
Arild Matsson <arild.matsson@gu.se>
2023-01-26 13:32:05 +0100

    Use FormKit for Create form

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
-import router from "./router";
 import App from "./App.vue";
-import "./index.css";
-import i18n from "./i18n/i18n";
-import { FontAwesomeIcon } from "./fontawesome";
+import router from "./router";
 import { createPinia } from "pinia";
+import i18n from "./i18n/i18n";
+import { defaultConfig, plugin } from "@formkit/vue";
+import { FontAwesomeIcon } from "./fontawesome";
+import "./index.css";
 
 const pinia = createPinia();
 
@@ -12,5 +13,18 @@ createApp(App) //
   .use(router)
   .use(pinia)
   .use(i18n)
+  .use(
+    plugin,
+    defaultConfig({
+      config: {
+        classes: {
+          outer: "my-2",
+          help: "text-sm py-1",
+          message: "text-red-500",
+          label: "font-medium",
+        },
+      },
+    })
+  )
   .component("icon", FontAwesomeIcon)
   .mount("#app");
