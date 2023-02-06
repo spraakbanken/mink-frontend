8ab68bbb14893be50c505e3d152964b369fa0ced
Arild Matsson <arild.matsson@gu.se>
2023-02-06 09:35:56 +0100

    Fix test

--- src/home/Home.test.js
+++ src/home/Home.test.js
@@ -3,12 +3,13 @@ import { test } from "vitest";
 import i18n from "@/i18n/i18n";
 import Home from "./Home.vue";
 import router from "@/router";
+import { formkit, formkitConfig } from "@/formkit";
 import { FontAwesomeIcon } from "@/fontawesome";
 
 test("frontpage", () => {
   const { getByText } = render(Home, {
     global: {
-      plugins: [i18n, router],
+      plugins: [i18n, router, [formkit, formkitConfig]],
       components: { icon: FontAwesomeIcon },
     },
   });
