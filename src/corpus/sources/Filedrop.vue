8b6ab34abde39cae50c135056d797084621da7b8
Arild Matsson <arild.matsson@gu.se>
2023-06-01 15:48:48 +0200

    Droptopage composable, #64

--- src/corpus/sources/Filedrop.vue
+++ src/corpus/sources/Filedrop.vue
@@ -5,20 +5,16 @@
 </template>
 
 <script setup>
-import { onMounted, onUnmounted } from "vue";
 import { useI18n } from "vue-i18n";
 import useMessenger from "@/message/messenger.composable";
-import { useToggle } from "@vueuse/core";
+import useDropToPage from "@/components/droptopage.composable";
 
 const emit = defineEmits(["drop"]);
 
 const { alert } = useMessenger();
 const { t } = useI18n();
-const [isDragover, setDragover] = useToggle();
 
 function drop(event) {
-  event.preventDefault();
-
   // On Chrome+Ubuntu, the file list may be empty for security reasons.
   // See https://askubuntu.com/a/1411727
   if (!event.dataTransfer.files[0]) {
@@ -27,27 +23,9 @@ function drop(event) {
   }
 
   emit("drop", event.dataTransfer.files);
-  setDragover(false);
 }
 
-const pageEl = document.querySelector("html");
-function onDragover(event) {
-  event.preventDefault();
-  setDragover(true);
-}
-function onDragleave() {
-  setDragover(false);
-}
-onMounted(() => {
-  pageEl.addEventListener("dragover", onDragover);
-  pageEl.addEventListener("dragleave", onDragleave);
-  pageEl.addEventListener("drop", drop);
-});
-onUnmounted(() => {
-  pageEl.removeEventListener("dragover", onDragover);
-  pageEl.removeEventListener("dragleave", onDragleave);
-  pageEl.removeEventListener("drop", drop);
-});
+const { isDragover } = useDropToPage(drop);
 </script>
 
 <style scoped>
