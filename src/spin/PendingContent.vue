17065680c49849ff1faf62d2908c13bca71945ba
Arild Matsson <arild.matsson@gu.se>
2023-07-11 14:49:37 +0200

    Make pulse animation clearer

--- src/spin/PendingContent.vue
+++ src/spin/PendingContent.vue
@@ -11,7 +11,7 @@ const { pending } = useSpin();
 </script>
 
 <template>
-  <div :class="{ 'animate-pulse': pending.includes(on) }">
+  <div :class="{ 'animate-pulse2': pending.includes(on) }">
     <slot />
   </div>
 </template>
