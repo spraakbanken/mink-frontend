26666d383f48901f9ae5a10099e3b8d05b64925e
Arild Matsson <arild.matsson@gu.se>
2023-07-11 14:58:46 +0200

    Show spinner on top of pending content

--- src/spin/PendingContent.vue
+++ src/spin/PendingContent.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Spinner from "./Spinner.vue";
 import useSpin from "./spin.composable";
 
 defineProps({
@@ -11,7 +12,13 @@ const { pending } = useSpin();
 </script>
 
 <template>
-  <div :class="{ 'animate-pulse2': pending.includes(on) }">
+  <div class="relative" :class="{ 'animate-pulse2': pending.includes(on) }">
+    <div
+      class="absolute top-0 left-0 right-0 bottom-0 z-30 flex justify-center items-center"
+      :class="{ hidden: !pending.includes(on) }"
+    >
+      <Spinner />
+    </div>
     <slot />
   </div>
 </template>
