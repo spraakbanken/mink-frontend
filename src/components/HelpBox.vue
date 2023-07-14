a2d4eaf418994dbe5d60fa85dd5306e79652fad8
Arild Matsson <arild.matsson@gu.se>
2023-07-14 12:11:40 +0200

    Help box gray unless action is needed

--- src/components/HelpBox.vue
+++ src/components/HelpBox.vue
@@ -1,6 +1,19 @@
+<script setup>
+defineProps({
+  important: {
+    type: Boolean,
+  },
+});
+</script>
+
 <template>
   <div
-    class="my-2 border-2 border-blue-300 bg-blue-100 dark:bg-sky-200 dark:border-0 dark:text-sky-800 rounded p-2 px-4 max-w-screen-sm"
+    class="my-2 rounded p-2 px-4 max-w-screen-sm"
+    :class="
+      important
+        ? ' bg-blue-100 border-2 border-blue-300 dark:bg-sky-200 dark:border-0 dark:text-sky-800'
+        : 'bg-stone-100 dark:bg-zinc-700 dark:border-0'
+    "
   >
     <slot />
   </div>
--- src/corpus/CorpusStateHelp.vue
+++ src/corpus/CorpusStateHelp.vue
@@ -4,11 +4,11 @@ import { useCorpusState } from "./corpusState.composable";
 import HelpBox from "@/components/HelpBox.vue";
 
 const corpusId = useCorpusIdParam();
-const { stateHelp } = useCorpusState(corpusId);
+const { stateHelp, isActionNeeded } = useCorpusState(corpusId);
 </script>
 
 <template>
-  <HelpBox v-if="stateHelp">
+  <HelpBox v-if="stateHelp" :important="isActionNeeded">
     <icon :icon="['far', 'lightbulb']" class="mr-1" />
     {{ stateHelp }}
   </HelpBox>
