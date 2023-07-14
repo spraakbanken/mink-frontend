4ff9d89e9471895498363bedf7b76218a763616f
Arild Matsson <arild.matsson@gu.se>
2023-07-14 17:28:05 +0200

    Spinner when creating corpus

--- src/corpora/Dashboard.vue
+++ src/corpora/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <Section :title="$t('corpuses')">
+  <Section v-if="isAuthenticated" :title="$t('corpuses')">
     <HelpBox>
       <p>
         {{
@@ -10,11 +10,7 @@
       </p>
     </HelpBox>
 
-    <PendingContent
-      v-if="isAuthenticated"
-      on="corpora"
-      class="flex flex-wrap -mx-2"
-    >
+    <PendingContent on="corpora" class="flex flex-wrap -mx-2">
       <router-link
         v-for="(corpus, corpusId) of corpusStore.corpora"
         :key="corpusId"
@@ -32,11 +28,13 @@
       </router-link>
     </PendingContent>
   </Section>
-  <Section :title="$t('new_corpus')">
-    <SourceUpload
-      :file-handler="createCorpusFromFiles"
-      :variant="corpusStore.hasCorpora ? null : 'primary'"
-    />
+  <Section v-if="isAuthenticated" :title="$t('new_corpus')">
+    <PendingContent on="create">
+      <SourceUpload
+        :file-handler="createCorpusFromFiles"
+        :variant="corpusStore.hasCorpora ? null : 'primary'"
+      />
+    </PendingContent>
   </Section>
 </template>
 
@@ -50,14 +48,16 @@ import SourceUpload from "@/corpus/sources/SourceUpload.vue";
 import HelpBox from "@/components/HelpBox.vue";
 import useCorpora from "@/corpora/corpora.composable";
 import { useCorpusStore } from "@/store/corpus.store";
+import useSpin from "@/spin/spin.composable";
 
 const corpusStore = useCorpusStore();
 const { requireAuthentication, isAuthenticated } = useAuth();
 const { loadCorpora, createFromUpload } = useCorpora();
+const { spin } = useSpin();
 
 requireAuthentication(loadCorpora);
 
 async function createCorpusFromFiles(files) {
-  await createFromUpload(files);
+  await spin(createFromUpload(files), null, "create");
 }
 </script>
