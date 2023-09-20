928695eb9009b50998ff444611d5bad8fe8d594e
Arild Matsson <arild.matsson@gu.se>
2023-09-20 14:24:45 +0200

    Improve layout in tool panel

--- src/corpus/exports/ToolPanel.vue
+++ src/corpus/exports/ToolPanel.vue
@@ -14,39 +14,40 @@ defineEmits(["install"]);
 </script>
 
 <template>
-  <div class="flex gap-2 my-2 justify-between items-baseline">
-    <div>
+  <div class="flex flex-col gap-2 my-4">
+    <div class="flex flex-wrap gap-2 justify-between items-baseline">
       <h3 class="font-bold">{{ name }}</h3>
-      <p class="text-sm text-gray-500 dark:text-gray-400">
-        {{ info }}
-        <a v-if="linkUrl && linkText" :href="linkUrl">
-          <ActionButton class="slim mute">
-            {{ linkText }}
+
+      <div class="flex gap-2 justify-end">
+        <ActionButton
+          :variant="canInstall && !isInstalled ? 'primary' : null"
+          :disabled="!canInstall"
+          class="whitespace-nowrap"
+          @click="canInstall ? $emit('install') : null"
+        >
+          {{
+            $t(
+              !isInstalled ? "exports.tools.install" : "exports.tools.reinstall"
+            )
+          }}
+        </ActionButton>
+
+        <a v-if="isInstalled" :href="showUrl" target="_blank">
+          <ActionButton variant="primary">
+            {{ $t("exports.tools.view") }}
           </ActionButton>
         </a>
-      </p>
-    </div>
-
-    <div class="flex flex-wrap gap-2 justify-end">
-      <ActionButton
-        :variant="canInstall && !isInstalled ? 'primary' : null"
-        :disabled="!canInstall"
-        class="whitespace-nowrap"
-        @click="canInstall ? $emit('install') : null"
-      >
-        {{
-          $t(!isInstalled ? "exports.tools.install" : "exports.tools.reinstall")
-        }}
-      </ActionButton>
-
-      <a v-if="isInstalled" :href="showUrl" target="_blank">
-        <ActionButton variant="primary">
+        <ActionButton v-else disabled>
           {{ $t("exports.tools.view") }}
         </ActionButton>
+      </div>
+    </div>
+
+    <div class="text-sm text-gray-500 dark:text-gray-400">
+      <div>{{ info }}</div>
+      <a v-if="linkUrl && linkText" :href="linkUrl">
+        {{ linkText }}
       </a>
-      <ActionButton v-else disabled>
-        {{ $t("exports.tools.view") }}
-      </ActionButton>
     </div>
   </div>
 </template>
