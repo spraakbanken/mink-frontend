315b10723a5926f17bd40c08a4bbc6b5a53eb0b2
Arild Matsson <arild.matsson@gu.se>
2024-01-02 14:57:19 +0100

    Use computed in PendingContent

--- src/spin/PendingContent.vue
+++ src/spin/PendingContent.vue
@@ -1,21 +1,23 @@
 <script setup>
+import { computed } from "vue";
 import Spinner from "./Spinner.vue";
 import useSpin from "./spin.composable";
 
-defineProps({
+const props = defineProps({
   on: {
     type: String,
     required: true,
   },
 });
 const { pending } = useSpin();
+const isPending = computed(() => pending.value.includes(props.on));
 </script>
 
 <template>
-  <div class="relative" :class="{ 'animate-pulse2': pending.includes(on) }">
+  <div class="relative" :class="{ 'animate-pulse2': isPending }">
     <div
       class="absolute top-0 left-0 right-0 bottom-0 z-30 flex justify-center items-center"
-      :class="{ hidden: !pending.includes(on) }"
+      :class="{ hidden: !isPending }"
     >
       <Spinner />
     </div>
