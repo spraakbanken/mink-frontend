cd408d6a4030ee856120cd16469e6dea8959a7ab
Arild Matsson <arild.matsson@gu.se>
2024-01-25 10:26:27 +0100

    Add missing MessageAlert.vue

---
+++ src/message/MessageAlert.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import useMessenger, { type MessageLevel } from "./messenger.composable";
+
+const props = defineProps<{
+  message: string;
+  level: MessageLevel;
+}>();
+
+const { alert } = useMessenger();
+
+alert(props.message, props.level);
+</script>
+
+<template>
+  <!-- Template has no effect, but Vue will warn in console if omitted. -->
+  <div class="message-alert"></div>
+</template>
