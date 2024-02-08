eda5d0e1e240ebe23691300efc051d93e3c09d09
Arild Matsson <arild.matsson@gu.se>
2024-02-08 13:06:36 +0100

    Show metadatas

--- src/corpora/LibraryView.vue
+++ src/corpora/LibraryView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import useLocale from "@/i18n/locale.composable";
 import PadButton from "@/components/PadButton.vue";
 import LayoutSection from "@/components/LayoutSection.vue";
 import PendingContent from "@/spin/PendingContent.vue";
@@ -11,12 +12,14 @@ import useCorpora from "@/corpora/corpora.composable";
 import { useCorpusStore } from "@/store/corpus.store";
 import useSpin from "@/spin/spin.composable";
 import useCreateCorpus from "@/corpus/createCorpus.composable";
+import RouteButton from "@/components/RouteButton.vue";
 
 const corpusStore = useCorpusStore();
 const { requireAuthentication, isAuthenticated } = useAuth();
 const { loadCorpora } = useCorpora();
 const { createFromUpload } = useCreateCorpus();
 const { spin } = useSpin();
+const { th } = useLocale();
 
 requireAuthentication(loadCorpora);
 
@@ -51,6 +54,17 @@ async function createCorpusFromFiles(files: FileList) {
       </PadButton>
     </PendingContent>
   </LayoutSection>
+
+  <LayoutSection :title="$t('metadata')">
+    <div class="flex flex-wrap gap-2">
+      <template v-for="(metadata, id) of corpusStore.metadatas" :key="id">
+        <RouteButton :to="`/resource/${id}`">
+          {{ th(metadata.name) || id }}
+        </RouteButton>
+      </template>
+    </div>
+  </LayoutSection>
+
   <LayoutSection v-if="isAuthenticated" :title="$t('new_corpus')">
     <PendingContent on="create" blocking>
       <SourceUpload
