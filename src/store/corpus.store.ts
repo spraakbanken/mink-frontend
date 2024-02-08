712694c16d36c962a977c90f0d1c2eecaa5be98b
Arild Matsson <arild.matsson@gu.se>
2024-02-08 13:06:24 +0100

    Distinguish corpora from metadata

--- src/store/corpus.store.ts
+++ src/store/corpus.store.ts
@@ -3,16 +3,33 @@ import { defineStore } from "pinia";
 import { useStorage } from "@vueuse/core";
 import { setKeys } from "@/util";
 import type { ByLang } from "@/util.types";
-import type { CorpusStatus, FileMeta, ResourceInfo } from "@/api/api.types";
+import type {
+  CorpusStatus,
+  FileMeta,
+  ResourceInfo,
+  ResourceType,
+} from "@/api/api.types";
 import type { ConfigOptions } from "@/api/corpusConfig";
 
-type Corpus = {
+type Resource = {
+  type: ResourceType;
   name: ByLang;
+};
+
+type Corpus = Resource & {
+  type: "corpus";
   sources: FileMeta[];
   config: ConfigOptions;
   status: CorpusStatus;
   exports: FileMeta[];
 };
+
+type Metadata = Resource;
+
+// A user-defined type guard to help inform TypeScript
+// See https://www.typescriptlang.org/docs/handbook/advanced-types.html#user-defined-type-guards
+const isCorpus = (resource: Partial<Resource>): resource is Corpus =>
+  resource.type == "corpus";
 
 export const useCorpusStore = defineStore("corpus", () => {
   // Connect state to browser's local storage. Change the number here to the
@@ -20,36 +37,65 @@ export const useCorpusStore = defineStore("corpus", () => {
   // forget the old state. The actual number doesn't really matter, as long as
   // it's a new one.
   const corporaRef = useStorage("mink@230102.corpora", {});
-  const corpora: Record<string, Partial<Corpus>> = reactive(corporaRef.value);
+  const resources: Record<string, Partial<Resource>> = reactive(
+    corporaRef.value,
+  );
 
-  function setCorpusIds(corpusIds: string[]) {
-    setKeys(corpora, corpusIds, {});
+  const corpora = computed<Record<string, Partial<Corpus>>>(() =>
+    filterResources("corpus"),
+  );
+  const metadatas = computed<Record<string, Partial<Metadata>>>(() =>
+    filterResources("metadata"),
+  );
+
+  const filterResources = <T extends Resource>(
+    type: ResourceType,
+  ): Record<string, Partial<T>> =>
+    Object.keys(resources).reduce(
+      (corpora, resourceId) =>
+        resources[resourceId].type == type
+          ? { ...corpora, [resourceId]: resources[resourceId] }
+          : corpora,
+      {},
+    );
+
+  function setCorpusIds(resourceIds: string[]) {
+    setKeys(resources, resourceIds, {});
   }
 
   function setCorpora(infos: ResourceInfo[]) {
     // Drop old keys, assign empty records for each new id
     const ids = infos.map((info) => info.resource.id);
-    setKeys(corpora, ids, {});
+    setKeys(resources, ids, {});
 
     for (const infoNew of infos) {
       // Patch any existing record, otherwise create a new one.
       const info =
-        infoNew.resource.id in corpora ? corpora[infoNew.resource.id] : {};
+        infoNew.resource.id in corpora.value
+          ? resources[infoNew.resource.id]
+          : {};
+      info.type = infoNew.resource.type;
       info.name = infoNew.resource.name;
-      info.sources = infoNew.resource.source_files;
-      info.status = infoNew.job;
-      corpora[infoNew.resource.id] = info;
+
+      if (isCorpus(info)) {
+        info.sources = infoNew.resource.source_files;
+        info.status = infoNew.job;
+      }
+
+      resources[infoNew.resource.id] = info;
     }
   }
 
   function removeCorpus(corpusId: string) {
-    delete corpora[corpusId];
+    delete resources[corpusId];
   }
 
   const hasCorpora = computed(() => !!Object.keys(corpora).length);
 
   return {
+    resources,
     corpora,
+    metadatas,
     setCorpusIds,
     setCorpora,
     removeCorpus,
