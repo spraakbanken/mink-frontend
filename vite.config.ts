d9ac6475d435622f59bf1a4760031cad3df46033
Arild Matsson <arild.matsson@gu.se>
2024-02-13 12:36:29 +0100

    Fix HTTPS optional

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 import ViteYaml from "@modyfi/vite-plugin-yaml";
 import { visualizer } from "rollup-plugin-visualizer";
-import { ServerOptions } from "https";
+import { ServerOptions as HttpsServerOptions } from "https";
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
@@ -11,11 +11,14 @@ export default defineConfig(async ({ mode }) => {
   Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
 
   // Read HTTPS cert and key, if their paths are specified in env.
-  const DEV_HTTPS: ServerOptions = {};
-  if (process.env.DEV_HTTPS_KEY && process.env.DEV_HTTPS_CERT) {
-    const fs = await import("fs");
-    DEV_HTTPS.key = fs.readFileSync(process.env.DEV_HTTPS_KEY);
-    DEV_HTTPS.cert = fs.readFileSync(process.env.DEV_HTTPS_CERT);
+  async function getHttpsOptions(): Promise<HttpsServerOptions | undefined> {
+    if (process.env.DEV_HTTPS_KEY && process.env.DEV_HTTPS_CERT) {
+      const fs = await import("fs");
+      return <HttpsServerOptions>{
+        key: fs.readFileSync(process.env.DEV_HTTPS_KEY),
+        cert: fs.readFileSync(process.env.DEV_HTTPS_CERT),
+      };
+    }
   }
 
   return {
@@ -34,7 +37,7 @@ export default defineConfig(async ({ mode }) => {
       // Remap hostname and enable HTTPS, in order for authentication to work.
       // Map this hostname to 127.0.0.1 in /etc/hosts.
       host: "minkdev.spraakbanken.gu.se",
-      https: DEV_HTTPS,
+      https: await getHttpsOptions(),
     },
     test: {
       environment: "happy-dom",
