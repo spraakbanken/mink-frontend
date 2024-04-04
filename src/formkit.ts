1c712c43d5959333112f2b0fb5733f635cd646f9
Arild Matsson <arild.matsson@gu.se>
2024-04-04 17:08:21 +0200

    Fix FormKit asterisk plugin

--- src/formkit.ts
+++ src/formkit.ts
@@ -3,43 +3,35 @@ import { en, sv } from "@formkit/i18n";
 export { plugin as formkit } from "@formkit/vue";
 import type { FormKitNode, FormKitExtendableSchemaRoot } from "@formkit/core";
 
+// Configure FormKit
 export const formkitConfig = defaultConfig({
   plugins: [addAsteriskPlugin],
   locales: { en, sv },
 });
 
-const isCheckboxAndRadioMultiple = (node: FormKitNode) =>
-  (node.props.type === "checkbox" || node.props.type === "radio") &&
-  node.props.options;
-
+/** A Formkit plugin that adds a red asterisk to required fields. */
 function addAsteriskPlugin(node: FormKitNode) {
-  const asteriskEl = {
-    $el: "span",
-    attrs: {
-      class: "mx-1 text-red-600 font-bold",
-    },
-    children: ["*"],
-  };
   node.on("created", () => {
     if (!node.props.definition) return;
-    const schemaFn = node.props.definition.schema;
-    node.props.definition.schema = (sectionsSchema = {}) => {
-      const isRequired = node.props.parsedRules.some(
-        (rule: any) => rule.name === "required",
-      );
+    const isCheckboxOrRadio = ["checkbox", "radio"].includes(node.props.type);
+    const isMultiple = node.props.options;
+    const propName = isCheckboxOrRadio && isMultiple ? "legend" : "label";
+    if (node.props.definition.schemaMemoKey)
+      node.props.definition.schemaMemoKey += `_asterisk_${propName}`;
 
-      if (isRequired) {
-        if (isCheckboxAndRadioMultiple(node)) {
-          sectionsSchema.legend = {
-            children: ["$label", asteriskEl],
-          };
-        } else {
-          sectionsSchema.label = {
-            children: ["$label", asteriskEl],
-          };
-        }
-      }
-      return (schemaFn as FormKitExtendableSchemaRoot)(sectionsSchema);
+    const asterisk = {
+      $el: "span",
+      if: "$state.required",
+      attrs: { class: "mx-1 text-red-600 font-bold" },
+      children: ["*"],
+    };
+
+    // Wrap and replace the schema function to decorate the schema with an asterisk span element if the field is required
+    const schemaFn = node.props.definition
+      .schema as FormKitExtendableSchemaRoot;
+    node.props.definition.schema = (sectionsSchema = {}) => {
+      sectionsSchema[propName] = { children: ["$label", asterisk] };
+      return schemaFn(sectionsSchema);
     };
   });
 }
