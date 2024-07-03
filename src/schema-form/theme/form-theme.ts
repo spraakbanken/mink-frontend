37d73459b2e1612d071633ad03df700cd329b60e
Arild Matsson <arild.matsson@gu.se>
2024-07-03 15:19:28 +0200

    Remove dummy field

--- src/schema-form/theme/form-theme.ts
+++ src/schema-form/theme/form-theme.ts
@@ -1,11 +1,7 @@
 import React from "react";
 import { applyPureVueInReact } from "veaury";
 import type { FormProps, ThemeProps } from "@rjsf/core";
-import type {
-  FieldProps,
-  IconButtonProps,
-  RegistryFieldsType,
-} from "@rjsf/utils";
+import type { IconButtonProps } from "@rjsf/utils";
 import type { Component } from "vue";
 import AddButtonVue from "./AddButton.vue";
 import RemoveButtonVue from "./RemoveButton.vue";
@@ -15,23 +11,6 @@ import MoveDownButtonVue from "./MoveDownButton.vue";
 // Rename and retype the Veaury converter to allow specifying return type better.
 const toReact = <P = {}>(component: Component) =>
   applyPureVueInReact(component) as React.ComponentType<P>;
-
-class MyField extends React.Component<FieldProps> {
-  constructor(props: FieldProps) {
-    super(props);
-    this.state = props.formData;
-  }
-
-  render() {
-    return React.createElement(
-      "div",
-      {},
-      `This is a MyField (${JSON.stringify(this.state)})`,
-    );
-  }
-}
-
-const fields: RegistryFieldsType = { BooleanField: MyField };
 
 const templates: FormProps["templates"] = {
   ButtonTemplates: {
@@ -43,7 +22,6 @@ const templates: FormProps["templates"] = {
 };
 
 const theme: ThemeProps = {
-  fields,
   templates,
 };
 export default theme;
