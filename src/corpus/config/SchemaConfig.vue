68dfa6ea01409fa79cba627caf1d3d2c58f48c34
Arild Matsson <arild.matsson@gu.se>
2025-02-03 10:18:56 +0100

    Omit sbx_strix because it has no schema

--- src/corpus/config/SchemaConfig.vue
+++ src/corpus/config/SchemaConfig.vue
@@ -2,7 +2,7 @@
 import { computed } from "vue";
 import Yaml from "js-yaml";
 import { useI18n } from "vue-i18n";
-import { difference } from "es-toolkit";
+import { difference, omit } from "es-toolkit";
 import {
   formSections,
   getTopProperties,
@@ -84,7 +84,7 @@ const uiSchemaModified = computed(() => ({
     <JsonSchemaForm
       v-if="configParsed"
       :schema
-      :data="configParsed"
+      :data="omit(configParsed, ['sbx_strix'])"
       :on-submit="onSubmit"
       :ui-schema="uiSchemaModified"
     />
