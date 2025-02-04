edeac2cfe6fff0a7b078c1e8f2ff31324376ad00
Arild Matsson <arild.matsson@gu.se>
2025-02-04 15:29:58 +0100

    feat: link to blog entry on source annotations on Home

--- src/home/HomeView.vue
+++ src/home/HomeView.vue
@@ -139,6 +139,17 @@ const logoutUrl = getLogoutUrl();
               scope="global"
               tag="p"
             />
+
+            <i18n-t keypath="home.features.upload.xml" scope="global" tag="p">
+              <template #link>
+                <a
+                  :href="$t('home.features.upload.xml.link.url')"
+                  target="_blank"
+                >
+                  {{ $t("home.features.upload.xml.link.label") }}
+                </a>
+              </template>
+            </i18n-t>
           </LayoutSection>
         </div>
 
