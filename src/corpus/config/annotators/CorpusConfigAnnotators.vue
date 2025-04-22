cdadf00c4a9b216b4bbf121b516aa1289c7596d9
Arild Matsson <arild.matsson@gu.se>
2025-04-22 16:49:25 +0200

    Move some hard code

--- src/corpus/config/annotators/CorpusConfigAnnotators.vue
+++ src/corpus/config/annotators/CorpusConfigAnnotators.vue
@@ -9,61 +9,19 @@ import CustomAnnotator from "./CustomAnnotator.vue";
 import ParameterField from "./ParameterField.vue";
 import ConfigField from "./ConfigField.vue";
 import * as A from "./annotators.types";
+import {
+  annotationOptions,
+  data,
+  decorateConfig,
+  findAnnotationDefs,
+  getCustom,
+  type Custom,
+  type CustomObject,
+  type DecoratedConfig,
+} from "./annotators";
 import LayoutBox from "@/components/LayoutBox.vue";
 import FormKitWrapper from "@/components/FormKitWrapper.vue";
 import TextData from "@/components/TextData.vue";
-import annotatorsFile from "@/assets/annotators.json";
-
-const data = (annotatorsFile as unknown as A.File).annotators;
-
-type Listing = {
-  key: string;
-  module: string;
-  moduleDef: A.Module;
-  func: string;
-  funcDef: A.Annotator;
-};
-
-type AnalysisListing = Listing & { funcDef: A.Analysis };
-type CustomListing = Listing & { type: "custom"; funcDef: A.Custom };
-type AnnotationListing = Listing & {
-  type: "annotation";
-  funcDef: A.Analysis;
-  annotation: string;
-  annotationDef: A.Annotation;
-};
-
-const annotators: Listing[] = Object.entries(data).flatMap(
-  ([module, moduleDef]) =>
-    Object.entries(moduleDef.functions).flatMap(([func, funcDef]) => ({
-      key: func,
-      module,
-      moduleDef,
-      func,
-      funcDef,
-    })),
-);
-
-const analyses = annotators.filter(({ funcDef }) =>
-  A.isAnalysis(funcDef),
-) as AnalysisListing[];
-const customs = annotators
-  .filter(({ funcDef }) => A.isCustom(funcDef))
-  .map((item) => ({ ...item, type: "custom" })) as CustomListing[];
-
-const annotations: AnnotationListing[] = analyses.flatMap((a) =>
-  Object.entries(a.funcDef.annotations).map(([annotation, annotationDef]) => ({
-    ...a,
-    type: "annotation",
-    key: annotation,
-    annotation,
-    annotationDef,
-  })),
-);
-
-const annotationOptions = [...annotations, ...customs].sort((a, b) =>
-  a.key.localeCompare(b.key),
-);
 
 const filterText = ref("ma");
 
@@ -91,40 +49,10 @@ const modulesFiltered = computed(() =>
     }),
   ),
 );
-const getAnalysis = (moduleName: string, functionName: string): A.Analysis => {
-  const annotator = data[moduleName].functions[functionName];
-  if (!A.isAnalysis(annotator)) throw new Error("Not an analysis");
-  return annotator;
-};
-
-const getCustom = (moduleName: string, functionName: string): A.Custom => {
-  const annotator = data[moduleName].functions[functionName];
-  if (!A.isCustom(annotator)) throw new Error("Not a custom annotator");
-  return annotator;
-};
 
 const selectedAnnotations = reactive<string[]>([
   "{child}:misc.inherit_{parent}_{attr}",
 ]);
-
-function findFunctions(annotation: string) {
-  const found: [string, string][] = [];
-  for (const module in data) {
-    const functions = data[module].functions;
-    for (const func in functions) {
-      if (A.isAnalysis(functions[func])) {
-        if (functions[func].annotations[annotation]) found.push([module, func]);
-      }
-    }
-  }
-  return found;
-}
-
-function findAnnotationDefs(annotation: string) {
-  return findFunctions(annotation).flatMap(([module, func]) =>
-    getAnalysis(module, func),
-  );
-}
 
 const wildcards = computed<Record<string, string[]>>(() =>
   Object.fromEntries(
@@ -157,9 +85,9 @@ watchImmediate(wildcards, () => {
 
 // TODO Optionally rename output with `as {name}`
 
-type Custom = { annotator: string; parameters: Record<string, unknown> };
 const selectedCustom = ref<Custom[]>([]);
-const selectedCustomObjects = computed(() =>
+
+const selectedCustomObjects = computed<CustomObject[]>(() =>
   selectedCustom.value.map(({ annotator: name, parameters }) => {
     const [moduleName, functionName] = name.split(":");
     const annotator = getCustom(moduleName, functionName);
@@ -253,18 +181,6 @@ function addCustom(name: string) {
     Object.keys(annotator.parameters).map((key) => [key, undefined]),
   );
   selectedCustom.value.push({ annotator: name, parameters });
-}
-
-type DecoratedConfig = A.Config & { _namespace: string; _name: string };
-function decorateConfig(
-  configMap: Record<string, A.Config>,
-): Record<string, DecoratedConfig> {
-  const decorated: Record<string, DecoratedConfig> = {};
-  for (const name in configMap) {
-    const [_namespace, _name] = name.split(".");
-    decorated[name] = { ...configMap[name], _namespace, _name };
-  }
-  return decorated;
 }
 </script>
 
---
+++ src/corpus/config/annotators/annotators.ts
@@ -0,0 +1,109 @@
+import * as A from "./annotators.types";
+import annotatorsFile from "@/assets/annotators.json";
+
+export const data = (annotatorsFile as unknown as A.File).annotators;
+
+type Listing = {
+  key: string;
+  module: string;
+  moduleDef: A.Module;
+  func: string;
+  funcDef: A.Annotator;
+};
+type AnalysisListing = Listing & { funcDef: A.Analysis };
+type CustomListing = Listing & { type: "custom"; funcDef: A.Custom };
+type AnnotationListing = Listing & {
+  type: "annotation";
+  funcDef: A.Analysis;
+  annotation: string;
+  annotationDef: A.Annotation;
+};
+export type Custom = { annotator: string; parameters: Record<string, unknown> };
+export type CustomObject = {
+  moduleName: string;
+  functionName: string;
+  annotator: A.Custom;
+  parameters: Record<string, unknown>;
+};
+export type DecoratedConfig = A.Config & { _namespace: string; _name: string };
+
+export const annotators: Listing[] = Object.entries(data).flatMap(
+  ([module, moduleDef]) =>
+    Object.entries(moduleDef.functions).flatMap(([func, funcDef]) => ({
+      key: func,
+      module,
+      moduleDef,
+      func,
+      funcDef,
+    })),
+);
+
+export const analyses = annotators.filter(({ funcDef }) =>
+  A.isAnalysis(funcDef),
+) as AnalysisListing[];
+
+export const customs = annotators
+  .filter(({ funcDef }) => A.isCustom(funcDef))
+  .map((item) => ({ ...item, type: "custom" })) as CustomListing[];
+
+export const annotations: AnnotationListing[] = analyses.flatMap((a) =>
+  Object.entries(a.funcDef.annotations).map(([annotation, annotationDef]) => ({
+    ...a,
+    type: "annotation",
+    key: annotation,
+    annotation,
+    annotationDef,
+  })),
+);
+
+export const annotationOptions = [...annotations, ...customs].sort((a, b) =>
+  a.key.localeCompare(b.key),
+);
+
+export const getAnalysis = (
+  moduleName: string,
+  functionName: string,
+): A.Analysis => {
+  const annotator = data[moduleName].functions[functionName];
+  if (!A.isAnalysis(annotator)) throw new Error("Not an analysis");
+  return annotator;
+};
+
+export const getCustom = (
+  moduleName: string,
+  functionName: string,
+): A.Custom => {
+  const annotator = data[moduleName].functions[functionName];
+  if (!A.isCustom(annotator)) throw new Error("Not a custom annotator");
+  return annotator;
+};
+
+function findFunctions(annotation: string) {
+  const found: [string, string][] = [];
+  for (const module in data) {
+    const functions = data[module].functions;
+    for (const func in functions) {
+      if (A.isAnalysis(functions[func])) {
+        if (functions[func].annotations[annotation]) found.push([module, func]);
+      }
+    }
+  }
+  return found;
+}
+
+export function findAnnotationDefs(annotation: string) {
+  return findFunctions(annotation).flatMap(([module, func]) =>
+    getAnalysis(module, func),
+  );
+}
+
+export function decorateConfig(
+  configMap: Record<string, A.Config>,
+): Record<string, DecoratedConfig> {
+  const decorated: Record<string, DecoratedConfig> = {};
+  for (const name in configMap) {
+    const [_namespace, _name] = name.split(".");
+    decorated[name] = { ...configMap[name], _namespace, _name };
+  }
+  return decorated;
+}
