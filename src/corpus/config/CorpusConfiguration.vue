e2aee9f62ff9b8f4d4640b75e639093191250d19
Arild Matsson <arild.matsson@gu.se>
2025-12-01 14:54:32 +0100

    Config panel headings like config page

--- src/corpus/config/CorpusConfiguration.vue
+++ src/corpus/config/CorpusConfiguration.vue
@@ -157,229 +157,233 @@ async function submit(fields: Form) {
 
 <template>
   <PendingContent :on="`corpus/${corpusId}/config`">
-    <TabsBar
-      :tabs="[
-        { key: 'metadata', label: $t('metadata') },
-        { key: 'settings', label: $t('settings') },
-        { key: 'analyses', label: $t('config.analyses') },
-      ]"
-      v-model="tabSelected"
-    />
+    <LayoutSection :title="$t('configuration')">
+      <TabsBar
+        :tabs="[
+          { key: 'metadata', label: $t('metadata') },
+          { key: 'settings', label: $t('settings') },
+          { key: 'analyses', label: $t('config.analyses') },
+        ]"
+        v-model="tabSelected"
+      />
 
-    <!-- Using the key attribute to re-render whole form after fetching config -->
-    <FormKitWrapper v-if="configOptions" :key="config">
-      <FormKit
-        id="corpus-config"
-        v-slot="{ value }"
-        type="form"
-        :submit-label="$t('save')"
-        :submit-attrs="{
-          inputClass: 'mink-button button-primary',
-        }"
-        @submit="submit"
-      >
-        <LayoutSection
-          :title="$t('metadata')"
-          v-show="tabSelected == 'metadata'"
+      <!-- Using the key attribute to re-render whole form after fetching config -->
+      <FormKitWrapper v-if="configOptions" :key="config">
+        <FormKit
+          id="corpus-config"
+          v-slot="{ value }"
+          type="form"
+          :submit-label="$t('save')"
+          :submit-attrs="{
+            inputClass: 'mink-button button-primary',
+          }"
+          @submit="submit"
         >
-          <HelpBox>
-            <p>{{ $t("config.metadata.help") }}</p>
-          </HelpBox>
+          <LayoutSection
+            :title="$t('metadata')"
+            v-show="tabSelected == 'metadata'"
+          >
+            <HelpBox>
+              <p>{{ $t("config.metadata.help") }}</p>
+            </HelpBox>
 
-          <div class="grid md:grid-cols-2 gap-4">
-            <LayoutBox
-              v-for="(lang2, lang3) of { swe: 'sv', eng: 'en' }"
-              :key="lang3"
-              :title="$t(lang2)"
+            <div class="grid md:grid-cols-2 gap-4">
+              <LayoutBox
+                v-for="(lang2, lang3) of { swe: 'sv', eng: 'en' }"
+                :key="lang3"
+                :title="$t(lang2)"
+              >
+                <FormKit type="group" name="name">
+                  <FormKit
+                    :name="lang3"
+                    :label="$t('name')"
+                    :value="configOptions.name?.[lang3]"
+                    :help="$t('metadata.name.help')"
+                    type="text"
+                    input-class="w-72"
+                  />
+                </FormKit>
+
+                <FormKit type="group" name="description">
+                  <FormKit
+                    :name="lang3"
+                    :label="$t('description')"
+                    :value="configOptions.description?.[lang3]"
+                    :help="$t('metadata.description.help')"
+                    type="textarea"
+                    input-class="w-full h-20"
+                  />
+                </FormKit>
+              </LayoutBox>
+            </div>
+
+            <FormKit
+              :label="$t('identifier')"
+              type="text"
+              name="identifier"
+              disabled
+              :value="corpusId"
+              :help="$t('metadata.identifier.help')"
             >
-              <FormKit type="group" name="name">
-                <FormKit
-                  :name="lang3"
-                  :label="$t('name')"
-                  :value="configOptions.name?.[lang3]"
-                  :help="$t('metadata.name.help')"
-                  type="text"
-                  input-class="w-72"
-                />
-              </FormKit>
-
-              <FormKit type="group" name="description">
-                <FormKit
-                  :name="lang3"
-                  :label="$t('description')"
-                  :value="configOptions.description?.[lang3]"
-                  :help="$t('metadata.description.help')"
-                  type="textarea"
-                  input-class="w-full h-20"
-                />
-              </FormKit>
-            </LayoutBox>
-          </div>
-
-          <FormKit
-            :label="$t('identifier')"
-            type="text"
-            name="identifier"
-            disabled
-            :value="corpusId"
-            :help="$t('metadata.identifier.help')"
-          >
-            <template #label>
-              <!-- Avoid orphaned <label> for better accessibility -->
-              <span class="formkit-label">{{ $t("identifier") }}</span>
-            </template>
-            <template #input>
-              <TerminalOutput class="inline leading-loose">
-                {{ corpusId }}
-              </TerminalOutput>
-            </template>
-          </FormKit>
-        </LayoutSection>
-
-        <LayoutSection
-          :title="$t('settings')"
-          v-show="tabSelected == 'settings'"
-        >
-          <HelpBox>
-            <p>{{ $t("config.configuration.help") }}</p>
-          </HelpBox>
-
-          <FormKit
-            name="format"
-            :label="$t('fileFormat')"
-            :value="selectedFormat"
-            type="select"
-            :placeholder="$t('select')"
-            input-class="w-72"
-            :options="formatOptions"
-            validation="required"
-            :help="$t('config.format.help')"
-          />
-
-          <HelpBox v-if="value!.format === 'pdf'" important>
-            <PhLightbulbFilament weight="bold" class="inline mb-1 mr-1" />
-            {{ $t("config.format.note.pdf") }}
-          </HelpBox>
-
-          <FormKit
-            v-if="value!.format === 'xml'"
-            name="textAnnotation"
-            :label="$t('config.text_annotation')"
-            type="text"
-            :value="configOptions.textAnnotation"
-            validation="required:trim|matches:/^[^<>\s]*$/"
-            input-class="w-40 font-mono"
-            :help="$t('config.text_annotation.help')"
-          >
-            <template #prefix> &lt; </template>
-            <template #suffix> &gt; </template>
-          </FormKit>
-
-          <FormKit
-            v-if="!['mp3', 'ogg', 'wav'].includes((value as Form).format)"
-            name="sentenceSegmenter"
-            :label="$t('segmenter_sentence')"
-            :value="configOptions.sentenceSegmenter || ''"
-            type="radio"
-            :options="segmenterOptions"
-            :help="$t('segmenter_sentence_help')"
-          />
-
-          <FormKit
-            name="datetimeFrom"
-            type="date"
-            :label="`${$t('timespan')}: ${$t('timespan_from')}`"
-            :value="configOptions.datetime?.from"
-            :max="(value as Form).datetimeTo"
-            validation="onlyif:datetimeTo"
-            :validation-messages="{
-              onlyif: $t('config.datetime.validate_both'),
-            }"
-          />
-          <FormKit
-            name="datetimeTo"
-            type="date"
-            :label="`${$t('timespan')}: ${$t('timespan_to')}`"
-            :value="configOptions.datetime?.to"
-            :min="(value as Form).datetimeFrom"
-            validation="onlyif:datetimeFrom"
-            :validation-messages="{
-              onlyif: $t('config.datetime.validate_both'),
-            }"
-            :help="$t('timespan_help')"
-          />
-        </LayoutSection>
-
-        <LayoutSection
-          :title="$t('config.analyses')"
-          v-show="tabSelected == 'analyses'"
-        >
-          <HelpBox>
-            <i18n-t keypath="config.analyses.info" scope="global">
-              <template #custom_config>
-                <router-link :to="`/library/corpus/${corpusId}/config/custom`">
-                  {{ $t("config.custom") }}
-                </router-link>
+              <template #label>
+                <!-- Avoid orphaned <label> for better accessibility -->
+                <span class="formkit-label">{{ $t("identifier") }}</span>
               </template>
-            </i18n-t>
-          </HelpBox>
+              <template #input>
+                <TerminalOutput class="inline leading-loose">
+                  {{ corpusId }}
+                </TerminalOutput>
+              </template>
+            </FormKit>
+          </LayoutSection>
 
-          <FormKit type="group" name="analyses">
-            <table class="my-2 striped">
-              <thead>
-                <tr>
-                  <th>{{ $t("description") }}</th>
-                  <th>{{ $t("identifier") }}</th>
-                  <th>{{ $t("config.analyses.task") }}</th>
-                </tr>
-              </thead>
-              <tbody v-for="(group, unit) in analyses" :key="unit">
-                <tr>
-                  <th colspan="5" class="text-lg pt-4!">
-                    {{ $t("config.analyses.unit") }}:
-                    {{ $t(`config.analyses.unit.${unit}`) }}
-                  </th>
-                </tr>
-                <tr v-for="analysis in group" :key="analysis.id">
-                  <td>
-                    <FormKit
-                      :name="analysis.id"
-                      :label="th(analysis.name)"
-                      :value="configOptions.analyses[analysis.id]"
-                      type="checkbox"
-                      :help="th(analysis.short_description)"
-                    />
-                  </td>
-                  <td>
-                    <a
-                      :href="$t('config.analyses.url', [analysis.id])"
-                      target="_blank"
-                      class="whitespace-nowrap"
-                    >
-                      {{ analysis.id }}
-                    </a>
-                  </td>
-                  <td>{{ th(analysis.task) }}</td>
-                </tr>
-              </tbody>
-            </table>
-          </FormKit>
-        </LayoutSection>
-      </FormKit>
-    </FormKitWrapper>
+          <LayoutSection
+            :title="$t('settings')"
+            v-show="tabSelected == 'settings'"
+          >
+            <HelpBox>
+              <p>{{ $t("config.configuration.help") }}</p>
+            </HelpBox>
 
-    <div class="flex justify-center items-baseline gap-4">
-      <RouteButton :to="`/library/corpus/${corpusId}/config/custom`">
-        {{ $t("config.custom") }}
-      </RouteButton>
+            <FormKit
+              name="format"
+              :label="$t('fileFormat')"
+              :value="selectedFormat"
+              type="select"
+              :placeholder="$t('select')"
+              input-class="w-72"
+              :options="formatOptions"
+              validation="required"
+              :help="$t('config.format.help')"
+            />
 
-      <RouteButton
-        :to="`/library/corpus/${corpusId}/delete`"
-        class="button-danger"
-      >
-        <PhTrash weight="fill" class="inline mb-1 mr-1" />
-        {{ $t("corpus.delete") }}
-      </RouteButton>
-    </div>
+            <HelpBox v-if="value!.format === 'pdf'" important>
+              <PhLightbulbFilament weight="bold" class="inline mb-1 mr-1" />
+              {{ $t("config.format.note.pdf") }}
+            </HelpBox>
+
+            <FormKit
+              v-if="value!.format === 'xml'"
+              name="textAnnotation"
+              :label="$t('config.text_annotation')"
+              type="text"
+              :value="configOptions.textAnnotation"
+              validation="required:trim|matches:/^[^<>\s]*$/"
+              input-class="w-40 font-mono"
+              :help="$t('config.text_annotation.help')"
+            >
+              <template #prefix> &lt; </template>
+              <template #suffix> &gt; </template>
+            </FormKit>
+
+            <FormKit
+              v-if="!['mp3', 'ogg', 'wav'].includes((value as Form).format)"
+              name="sentenceSegmenter"
+              :label="$t('segmenter_sentence')"
+              :value="configOptions.sentenceSegmenter || ''"
+              type="radio"
+              :options="segmenterOptions"
+              :help="$t('segmenter_sentence_help')"
+            />
+
+            <FormKit
+              name="datetimeFrom"
+              type="date"
+              :label="`${$t('timespan')}: ${$t('timespan_from')}`"
+              :value="configOptions.datetime?.from"
+              :max="(value as Form).datetimeTo"
+              validation="onlyif:datetimeTo"
+              :validation-messages="{
+                onlyif: $t('config.datetime.validate_both'),
+              }"
+            />
+            <FormKit
+              name="datetimeTo"
+              type="date"
+              :label="`${$t('timespan')}: ${$t('timespan_to')}`"
+              :value="configOptions.datetime?.to"
+              :min="(value as Form).datetimeFrom"
+              validation="onlyif:datetimeFrom"
+              :validation-messages="{
+                onlyif: $t('config.datetime.validate_both'),
+              }"
+              :help="$t('timespan_help')"
+            />
+          </LayoutSection>
+
+          <LayoutSection
+            :title="$t('config.analyses')"
+            v-show="tabSelected == 'analyses'"
+          >
+            <HelpBox>
+              <i18n-t keypath="config.analyses.info" scope="global">
+                <template #custom_config>
+                  <router-link
+                    :to="`/library/corpus/${corpusId}/config/custom`"
+                  >
+                    {{ $t("config.custom") }}
+                  </router-link>
+                </template>
+              </i18n-t>
+            </HelpBox>
+
+            <FormKit type="group" name="analyses">
+              <table class="my-2 striped">
+                <thead>
+                  <tr>
+                    <th>{{ $t("description") }}</th>
+                    <th>{{ $t("identifier") }}</th>
+                    <th>{{ $t("config.analyses.task") }}</th>
+                  </tr>
+                </thead>
+                <tbody v-for="(group, unit) in analyses" :key="unit">
+                  <tr>
+                    <th colspan="5" class="text-lg pt-4!">
+                      {{ $t("config.analyses.unit") }}:
+                      {{ $t(`config.analyses.unit.${unit}`) }}
+                    </th>
+                  </tr>
+                  <tr v-for="analysis in group" :key="analysis.id">
+                    <td>
+                      <FormKit
+                        :name="analysis.id"
+                        :label="th(analysis.name)"
+                        :value="configOptions.analyses[analysis.id]"
+                        type="checkbox"
+                        :help="th(analysis.short_description)"
+                      />
+                    </td>
+                    <td>
+                      <a
+                        :href="$t('config.analyses.url', [analysis.id])"
+                        target="_blank"
+                        class="whitespace-nowrap"
+                      >
+                        {{ analysis.id }}
+                      </a>
+                    </td>
+                    <td>{{ th(analysis.task) }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </FormKit>
+          </LayoutSection>
+        </FormKit>
+      </FormKitWrapper>
+
+      <div class="flex justify-center items-baseline gap-4">
+        <RouteButton :to="`/library/corpus/${corpusId}/config/custom`">
+          {{ $t("config.custom") }}
+        </RouteButton>
+
+        <RouteButton
+          :to="`/library/corpus/${corpusId}/delete`"
+          class="button-danger"
+        >
+          <PhTrash weight="fill" class="inline mb-1 mr-1" />
+          {{ $t("corpus.delete") }}
+        </RouteButton>
+      </div>
+    </LayoutSection>
   </PendingContent>
 </template>
