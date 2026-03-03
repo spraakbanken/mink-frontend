2b6b5d4636a47d4c9d5b80fca9f2d8709cb15194
Arild Matsson <arild.matsson@gu.se>
2026-03-03 14:03:00 +0100

    fix: add job_status

--- src/api/api.ts
+++ src/api/api.ts
@@ -202,7 +202,7 @@ class MinkApi {
   }
 
   /** @see https://ws.spraakbanken.gu.se/docs/mink#tag/Manage-Metadata/operation/download-metadata-yaml-get */
-  async downloadMetaataYaml(id: string) {
+  async downloadMetadataYaml(id: string) {
     const response = await this.axios
       .get<string>("download-metadata-yaml", {
         params: { corpus_id: id },
