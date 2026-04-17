136bb70a9cf56036787fe1790d1e4964df56c11e
Arild Matsson <arild.matsson@gu.se>
2026-04-17 15:29:29 +0200

    Extract ResourceViewTemplate

--- src/corpus/CorpusView.vue
+++ src/corpus/CorpusView.vue
@@ -1,39 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import useResourceIdParam from "@/resource/resourceIdParam.composable";
-import PageTitle from "@/components/PageTitle.vue";
-import useLocale from "@/i18n/locale.composable";
-import { useResourceStore } from "@/store/resource.store";
-import { type Corpus } from "@/store/resource.types";
-import useMessenger from "@/message/messenger.composable";
-import useNotFound from "@/components/notfound.composable";
+import ResourceViewTemplate from "@/resource/ResourceViewTemplate.vue";
 
 const id = useResourceIdParam();
-const { loadTypedResource } = useResourceStore();
-const { th } = useLocale();
-const { alertError } = useMessenger();
-const { handle404 } = useNotFound();
-
-const corpus = ref<Corpus>();
-
-loadTypedResource("corpus", id)
-  .then((resource) => (corpus.value = resource))
-  .catch(handle404)
-  .catch(alertError);
 </script>
 
 <template>
-  <PageTitle subtitle="corpus">
-    <router-link
-      v-if="$route.path != `/library/corpus/${id}`"
-      :to="`/library/corpus/${id}`"
-      class="text-inherit no-underline hover:underline"
-    >
-      {{ th(corpus?.name) || id }}
-    </router-link>
-    <template v-else>
-      {{ th(corpus?.name) || id }}
-    </template>
-  </PageTitle>
-  <router-view v-if="corpus" />
+  <ResourceViewTemplate type="corpus" :id />
 </template>
--- src/lexicon/LexiconView.vue
+++ src/lexicon/LexiconView.vue
@@ -1,39 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import useNotFound from "@/components/notfound.composable";
-import PageTitle from "@/components/PageTitle.vue";
-import useLocale from "@/i18n/locale.composable";
-import useMessenger from "@/message/messenger.composable";
 import useResourceIdParam from "@/resource/resourceIdParam.composable";
-import { type Lexicon } from "@/store/resource.types";
-import { useResourceStore } from "@/store/resource.store";
+import ResourceViewTemplate from "@/resource/ResourceViewTemplate.vue";
 
 const id = useResourceIdParam();
-const { loadTypedResource } = useResourceStore();
-const { th } = useLocale();
-const { alertError } = useMessenger();
-const { handle404 } = useNotFound();
-
-const lexicon = ref<Lexicon>();
-
-loadTypedResource("lexicon", id)
-  .then((resource) => (lexicon.value = resource))
-  .catch(handle404)
-  .catch(alertError);
 </script>
 
 <template>
-  <PageTitle subtitle="lexicon">
-    <router-link
-      v-if="$route.path != `/library/lexicon/${id}`"
-      :to="`/library/lexicon/${id}`"
-      class="text-inherit no-underline hover:underline"
-    >
-      {{ th(lexicon?.name) || id }}
-    </router-link>
-    <template v-else>
-      {{ th(lexicon?.name) || id }}
-    </template>
-  </PageTitle>
-  <router-view v-if="lexicon" />
+  <ResourceViewTemplate type="lexicon" :id />
 </template>
